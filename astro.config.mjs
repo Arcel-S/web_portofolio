3a02b57cb1927b9948452b11258f7491b3090166
Arcel-S <marcellsiregar1204@gmail.com>
2026-07-23 22:29:33 +0700

    chore(deploy): prepare production build for Vercel, integrate sitemap, and clean gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://namaanda.dev',
@@ -11,6 +12,7 @@ export default defineConfig({
     }),
     mdx(),
     react(),
+    sitemap(),
   ],
   prefetch: true,
 });
